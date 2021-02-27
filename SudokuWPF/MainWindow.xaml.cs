ca8866205685295ac2cc6ad91e8c5483f4f94079
MLD0120 <laszlomocsy.dev@gmail.com>
2021-02-27 22:55:23 +0100

    Updated MainWindow.xaml.cs

--- SudokuWPF/MainWindow.xaml.cs
+++ SudokuWPF/MainWindow.xaml.cs
@@ -26,15 +26,15 @@ namespace SudokuWPF
         Brush ColorDefault = Brushes.Black;
         Brush ColorUser = Brushes.Blue;
         List<char> Chars = new List<char> { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-        List<char> BoxAAhars = new List<char>();
-        List<char> BoxABhars = new List<char>();
-        List<char> BoxAChars = new List<char>();
-        List<char> BoxBAhars = new List<char>();
-        List<char> BoxBBhars = new List<char>();
-        List<char> BoxBChars = new List<char>();
-        List<char> BoxCAhars = new List<char>();
-        List<char> BoxCBhars = new List<char>();
-        List<char> BoxCChars = new List<char>();
+        List<char> BoxAAChars = new List<char>();
+        List<char> BoxABChars = new List<char>();
+        List<char> BoxACChars = new List<char>();
+        List<char> BoxBAChars = new List<char>();
+        List<char> BoxBBChars = new List<char>();
+        List<char> BoxBCChars = new List<char>();
+        List<char> BoxCAChars = new List<char>();
+        List<char> BoxCBChars = new List<char>();
+        List<char> BoxCCChars = new List<char>();
 
         public MainWindow()
         {
@@ -58,29 +58,29 @@ namespace SudokuWPF
             ItemList.Add(ItemAA);
             ItemList.Add(ItemAB);
             ItemList.Add(ItemAC);
-            ItemList.Add(ItemAD);
-            ItemList.Add(ItemAE);
-            ItemList.Add(ItemAF);
-            ItemList.Add(ItemAG);
-            ItemList.Add(ItemAH);
-            ItemList.Add(ItemAI);
-
             ItemList.Add(ItemBA);
             ItemList.Add(ItemBB);
             ItemList.Add(ItemBC);
-            ItemList.Add(ItemBD);
-            ItemList.Add(ItemBE);
-            ItemList.Add(ItemBF);
-            ItemList.Add(ItemBG);
-            ItemList.Add(ItemBH);
-            ItemList.Add(ItemBI);
-
             ItemList.Add(ItemCA);
             ItemList.Add(ItemCB);
             ItemList.Add(ItemCC);
+
+            ItemList.Add(ItemAD);
+            ItemList.Add(ItemAE);
+            ItemList.Add(ItemAF);
+            ItemList.Add(ItemBD);
+            ItemList.Add(ItemBE);
+            ItemList.Add(ItemBF);
             ItemList.Add(ItemCD);
             ItemList.Add(ItemCE);
             ItemList.Add(ItemCF);
+
+            ItemList.Add(ItemAG);
+            ItemList.Add(ItemAH);
+            ItemList.Add(ItemAI);
+            ItemList.Add(ItemBG);
+            ItemList.Add(ItemBH);
+            ItemList.Add(ItemBI);
             ItemList.Add(ItemCG);
             ItemList.Add(ItemCH);
             ItemList.Add(ItemCI);
@@ -88,29 +88,29 @@ namespace SudokuWPF
             ItemList.Add(ItemDA);
             ItemList.Add(ItemDB);
             ItemList.Add(ItemDC);
-            ItemList.Add(ItemDD);
-            ItemList.Add(ItemDE);
-            ItemList.Add(ItemDF);
-            ItemList.Add(ItemDG);
-            ItemList.Add(ItemDH);
-            ItemList.Add(ItemDI);
-
             ItemList.Add(ItemEA);
             ItemList.Add(ItemEB);
             ItemList.Add(ItemEC);
-            ItemList.Add(ItemED);
-            ItemList.Add(ItemEE);
-            ItemList.Add(ItemEF);
-            ItemList.Add(ItemEG);
-            ItemList.Add(ItemEH);
-            ItemList.Add(ItemEI);
-
             ItemList.Add(ItemFA);
             ItemList.Add(ItemFB);
             ItemList.Add(ItemFC);
+
+            ItemList.Add(ItemDD);
+            ItemList.Add(ItemDE);
+            ItemList.Add(ItemDF);
+            ItemList.Add(ItemED);
+            ItemList.Add(ItemEE);
+            ItemList.Add(ItemEF);
             ItemList.Add(ItemFD);
             ItemList.Add(ItemFE);
             ItemList.Add(ItemFF);
+
+            ItemList.Add(ItemDG);
+            ItemList.Add(ItemDH);
+            ItemList.Add(ItemDI);
+            ItemList.Add(ItemEG);
+            ItemList.Add(ItemEH);
+            ItemList.Add(ItemEI);
             ItemList.Add(ItemFG);
             ItemList.Add(ItemFH);
             ItemList.Add(ItemFI);
@@ -118,29 +118,29 @@ namespace SudokuWPF
             ItemList.Add(ItemGA);
             ItemList.Add(ItemGB);
             ItemList.Add(ItemGC);
-            ItemList.Add(ItemGD);
-            ItemList.Add(ItemGE);
-            ItemList.Add(ItemGF);
-            ItemList.Add(ItemGG);
-            ItemList.Add(ItemGH);
-            ItemList.Add(ItemGI);
-
             ItemList.Add(ItemHA);
             ItemList.Add(ItemHB);
             ItemList.Add(ItemHC);
-            ItemList.Add(ItemHD);
-            ItemList.Add(ItemHE);
-            ItemList.Add(ItemHF);
-            ItemList.Add(ItemHG);
-            ItemList.Add(ItemHH);
-            ItemList.Add(ItemHI);
-
             ItemList.Add(ItemIA);
             ItemList.Add(ItemIB);
             ItemList.Add(ItemIC);
+
+            ItemList.Add(ItemGD);
+            ItemList.Add(ItemGE);
+            ItemList.Add(ItemGF);
+            ItemList.Add(ItemHD);
+            ItemList.Add(ItemHE);
+            ItemList.Add(ItemHF);
             ItemList.Add(ItemID);
             ItemList.Add(ItemIE);
             ItemList.Add(ItemIF);
+
+            ItemList.Add(ItemGG);
+            ItemList.Add(ItemGH);
+            ItemList.Add(ItemGI);
+            ItemList.Add(ItemHG);
+            ItemList.Add(ItemHH);
+            ItemList.Add(ItemHI);
             ItemList.Add(ItemIG);
             ItemList.Add(ItemIH);
             ItemList.Add(ItemII);
@@ -189,6 +189,7 @@ namespace SudokuWPF
         {
             this.IsEnabled = false;
 
+            int _BoxIndex = 1;
             foreach(Button button in ItemList)
             {
                 if(button.Content.ToString() == "")
@@ -196,18 +197,49 @@ namespace SudokuWPF
                     ItemSolverList.Add(button);
                     Console.WriteLine(button.Name);
                 }
+                else
+                {
+                    if(_BoxIndex <= 9)
+                    {
+                        BoxAAChars.Add(Convert.ToChar(button.Content));
+                    }
+                    else if(10 <= _BoxIndex && _BoxIndex <= 18)
+                    {
+                        BoxABChars.Add(Convert.ToChar(button.Content));
+                    }
+                    else if (19 <= _BoxIndex && _BoxIndex <= 27)
+                    {
+                        BoxACChars.Add(Convert.ToChar(button.Content));
+                    }
+                    else if (28 <= _BoxIndex && _BoxIndex <= 36)
+                    {
+                        BoxBAChars.Add(Convert.ToChar(button.Content));
+                    }
+                    else if (37 <= _BoxIndex && _BoxIndex <= 45)
+                    {
+                        BoxBBChars.Add(Convert.ToChar(button.Content));
+                    }
+                    else if (46 <= _BoxIndex && _BoxIndex <= 54)
+                    {
+                        BoxBCChars.Add(Convert.ToChar(button.Content));
+                    }
+                    else if (55 <= _BoxIndex && _BoxIndex <= 63)
+                    {
+                        BoxCAChars.Add(Convert.ToChar(button.Content));
+                    }
+                    else if (64 <= _BoxIndex && _BoxIndex <= 72)
+                    {
+                        BoxCBChars.Add(Convert.ToChar(button.Content));
+                    }
+                    else if (73 <= _BoxIndex && _BoxIndex <= 81)
+                    {
+                        BoxCCChars.Add(Convert.ToChar(button.Content));
+                    }
+                }
+                _BoxIndex++;
             }
-            Item_MissingChars();
 
             this.IsEnabled = true;
-        }
-
-        void Item_MissingChars()
-        {
-            foreach(char _char in Chars)
-            {
-
-            }
         }
     }
 }
