093a07c0e58d6faf3400056ef332ea6fdc5c2283
MLD0120 <laszlomocsy.dev@gmail.com>
2021-02-27 22:35:26 +0100

    Updated MainWondow.xaml.cs

--- SudokuWPF/MainWindow.xaml.cs
+++ SudokuWPF/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Threading;
 
 namespace SudokuWPF
 {
@@ -24,6 +25,16 @@ namespace SudokuWPF
         List<Button> ItemSolverList = new List<Button>();
         Brush ColorDefault = Brushes.Black;
         Brush ColorUser = Brushes.Blue;
+        List<char> Chars = new List<char> { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        List<char> BoxAAhars = new List<char>();
+        List<char> BoxABhars = new List<char>();
+        List<char> BoxAChars = new List<char>();
+        List<char> BoxBAhars = new List<char>();
+        List<char> BoxBBhars = new List<char>();
+        List<char> BoxBChars = new List<char>();
+        List<char> BoxCAhars = new List<char>();
+        List<char> BoxCBhars = new List<char>();
+        List<char> BoxCChars = new List<char>();
 
         public MainWindow()
         {
@@ -42,7 +53,7 @@ namespace SudokuWPF
             //Console.WriteLine(Item);
         }
 
-        private void Load_ItemList()
+        void Load_ItemList()
         {
             ItemList.Add(ItemAA);
             ItemList.Add(ItemAB);
@@ -177,108 +188,26 @@ namespace SudokuWPF
         private void Solve_Clicked(object sender, RoutedEventArgs e)
         {
             this.IsEnabled = false;
-            List<string> NumberLine = new List<string>();
 
             foreach(Button button in ItemList)
             {
                 if(button.Content.ToString() == "")
                 {
                     ItemSolverList.Add(button);
-                    NumberLine.Add("1");
                     Console.WriteLine(button.Name);
                 }
             }
-            Console.WriteLine(NumberLine.Count());
-
-            bool solved = false;
-            List<string> _ItemValueList = new List<string>();
-            while (!solved)
-            {
-                _ItemValueList.Clear();
-                int ItemNumber = 0;
-                foreach (Button button in ItemList)
-                {
-                    if (ItemSolverList.Contains(button))
-                    {
-                        Refresh(button, NumberLine[ItemNumber], true);
-                        _ItemValueList.Add(button.Content.ToString());
-                    }
-                    else
-                    {
-                        _ItemValueList.Add(button.Content.ToString());
-                    }
-                }
-
-                solved = SudokuProcess(_ItemValueList);
-
-            }
+            Item_MissingChars();
 
             this.IsEnabled = true;
         }
 
-        public bool SudokuProcess(List<string> ItemValueList)
+        void Item_MissingChars()
         {
-            string[] Chars = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
-            string[] HorizontalA = { ItemValueList[0], ItemValueList[1], ItemValueList[2], ItemValueList[3], ItemValueList[4], ItemValueList[5], ItemValueList[6], ItemValueList[7], ItemValueList[8] };
-            string[] HorizontalB = { ItemValueList[9], ItemValueList[10], ItemValueList[11], ItemValueList[12], ItemValueList[13], ItemValueList[14], ItemValueList[15], ItemValueList[16], ItemValueList[17] };
-            string[] HorizontalC = { ItemValueList[18], ItemValueList[19], ItemValueList[20], ItemValueList[21], ItemValueList[22], ItemValueList[23], ItemValueList[24], ItemValueList[25], ItemValueList[26] };
-            string[] HorizontalD = { ItemValueList[27], ItemValueList[28], ItemValueList[29], ItemValueList[30], ItemValueList[31], ItemValueList[32], ItemValueList[33], ItemValueList[34], ItemValueList[35] };
-            string[] HorizontalE = { ItemValueList[36], ItemValueList[37], ItemValueList[38], ItemValueList[39], ItemValueList[40], ItemValueList[41], ItemValueList[42], ItemValueList[43], ItemValueList[44] };
-            string[] HorizontalF = { ItemValueList[45], ItemValueList[46], ItemValueList[47], ItemValueList[48], ItemValueList[49], ItemValueList[50], ItemValueList[51], ItemValueList[52], ItemValueList[53] };
-            string[] HorizontalG = { ItemValueList[54], ItemValueList[55], ItemValueList[56], ItemValueList[57], ItemValueList[58], ItemValueList[59], ItemValueList[60], ItemValueList[61], ItemValueList[62] };
-            string[] HorizontalH = { ItemValueList[63], ItemValueList[64], ItemValueList[65], ItemValueList[66], ItemValueList[67], ItemValueList[68], ItemValueList[69], ItemValueList[70], ItemValueList[71] };
-            string[] HorizontalI = { ItemValueList[72], ItemValueList[73], ItemValueList[74], ItemValueList[75], ItemValueList[76], ItemValueList[77], ItemValueList[78], ItemValueList[79], ItemValueList[80] };
-            string[] VerticalA = { ItemValueList[0], ItemValueList[9], ItemValueList[18], ItemValueList[27], ItemValueList[36], ItemValueList[45], ItemValueList[54], ItemValueList[63], ItemValueList[72] };
-            string[] VerticalB = { ItemValueList[1], ItemValueList[10], ItemValueList[19], ItemValueList[28], ItemValueList[37], ItemValueList[46], ItemValueList[55], ItemValueList[64], ItemValueList[73] };
-            string[] VerticalC = { ItemValueList[2], ItemValueList[11], ItemValueList[20], ItemValueList[29], ItemValueList[38], ItemValueList[47], ItemValueList[56], ItemValueList[65], ItemValueList[74] };
-            string[] VerticalD = { ItemValueList[3], ItemValueList[12], ItemValueList[21], ItemValueList[30], ItemValueList[39], ItemValueList[48], ItemValueList[57], ItemValueList[66], ItemValueList[75] };
-            string[] VerticalE = { ItemValueList[4], ItemValueList[13], ItemValueList[22], ItemValueList[31], ItemValueList[40], ItemValueList[49], ItemValueList[58], ItemValueList[67], ItemValueList[76] };
-            string[] VerticalF = { ItemValueList[5], ItemValueList[14], ItemValueList[23], ItemValueList[32], ItemValueList[41], ItemValueList[50], ItemValueList[59], ItemValueList[68], ItemValueList[77] };
-            string[] VerticalG = { ItemValueList[6], ItemValueList[15], ItemValueList[24], ItemValueList[33], ItemValueList[42], ItemValueList[51], ItemValueList[60], ItemValueList[69], ItemValueList[78] };
-            string[] VerticalH = { ItemValueList[7], ItemValueList[16], ItemValueList[25], ItemValueList[34], ItemValueList[43], ItemValueList[52], ItemValueList[61], ItemValueList[70], ItemValueList[79] };
-            string[] VerticalI = { ItemValueList[8], ItemValueList[17], ItemValueList[26], ItemValueList[35], ItemValueList[44], ItemValueList[53], ItemValueList[62], ItemValueList[71], ItemValueList[80] };
-            string[] BoxA = { ItemValueList[0], ItemValueList[1], ItemValueList[2], ItemValueList[9], ItemValueList[10], ItemValueList[11], ItemValueList[18], ItemValueList[19], ItemValueList[20] };
-            string[] BoxB = { ItemValueList[3], ItemValueList[4], ItemValueList[5], ItemValueList[12], ItemValueList[13], ItemValueList[14], ItemValueList[21], ItemValueList[22], ItemValueList[23] };
-            string[] BoxC = { ItemValueList[6], ItemValueList[7], ItemValueList[8], ItemValueList[15], ItemValueList[16], ItemValueList[17], ItemValueList[24], ItemValueList[25], ItemValueList[26] };
-            string[] BoxD = { ItemValueList[27], ItemValueList[28], ItemValueList[29], ItemValueList[36], ItemValueList[37], ItemValueList[38], ItemValueList[45], ItemValueList[46], ItemValueList[47] };
-            string[] BoxE = { ItemValueList[30], ItemValueList[31], ItemValueList[32], ItemValueList[39], ItemValueList[40], ItemValueList[41], ItemValueList[48], ItemValueList[49], ItemValueList[50] };
-            string[] BoxF = { ItemValueList[33], ItemValueList[34], ItemValueList[35], ItemValueList[42], ItemValueList[43], ItemValueList[44], ItemValueList[51], ItemValueList[52], ItemValueList[53] };
-            string[] BoxG = { ItemValueList[54], ItemValueList[55], ItemValueList[56], ItemValueList[63], ItemValueList[64], ItemValueList[65], ItemValueList[72], ItemValueList[73], ItemValueList[74] };
-            string[] BoxH = { ItemValueList[57], ItemValueList[58], ItemValueList[59], ItemValueList[66], ItemValueList[67], ItemValueList[68], ItemValueList[75], ItemValueList[76], ItemValueList[77] };
-            string[] BoxI = { ItemValueList[60], ItemValueList[61], ItemValueList[62], ItemValueList[69], ItemValueList[70], ItemValueList[71], ItemValueList[78], ItemValueList[79], ItemValueList[80] };
+            foreach(char _char in Chars)
+            {
 
-            foreach(string _char in Chars){
-                if (!HorizontalA.Contains(_char)) { return false; }
-                if (!HorizontalB.Contains(_char)) { return false; }
-                if (!HorizontalC.Contains(_char)) { return false; }
-                if (!HorizontalD.Contains(_char)) { return false; }
-                if (!HorizontalE.Contains(_char)) { return false; }
-                if (!HorizontalF.Contains(_char)) { return false; }
-                if (!HorizontalG.Contains(_char)) { return false; }
-                if (!HorizontalH.Contains(_char)) { return false; }
-                if (!HorizontalI.Contains(_char)) { return false; }
-                if (!VerticalA.Contains(_char)) { return false; }
-                if (!VerticalB.Contains(_char)) { return false; }
-                if (!VerticalC.Contains(_char)) { return false; }
-                if (!VerticalD.Contains(_char)) { return false; }
-                if (!VerticalE.Contains(_char)) { return false; }
-                if (!VerticalF.Contains(_char)) { return false; }
-                if (!VerticalG.Contains(_char)) { return false; }
-                if (!VerticalH.Contains(_char)) { return false; }
-                if (!VerticalI.Contains(_char)) { return false; }
-                if (!BoxA.Contains(_char)) { return false; }
-                if (!BoxB.Contains(_char)) { return false; }
-                if (!BoxC.Contains(_char)) { return false; }
-                if (!BoxD.Contains(_char)) { return false; }
-                if (!BoxE.Contains(_char)) { return false; }
-                if (!BoxF.Contains(_char)) { return false; }
-                if (!BoxG.Contains(_char)) { return false; }
-                if (!BoxH.Contains(_char)) { return false; }
-                if (!BoxI.Contains(_char)) { return false; }
             }
-
-            Console.WriteLine("SUDOKU SOLVED!!!");
-            return true; //Sudoku solved!
         }
     }
 }
